e7e1c74022a8c74544dea48dd6c42896a25b22b2
Ogbuome <preciousudo74@gmail.com>
2023-03-01 18:15:35 -0800

    main.c

---
+++ 0x06-pointers_arrays_strings/main.c
@@ -0,0 +1,18 @@
+i#ifndef FILE MAIN
+#define FILE_MAIN
+
+int putchar(char c);
+char strcat (char dest, char *src); 
+char strncat (char *dest, char *src, int n); 
+char strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n); 
+char *string toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number (intn); 
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer (char *b, int size);
+
+#endif
